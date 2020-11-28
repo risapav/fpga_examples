eac369c92764517cfcda7051c5963ae5240d3b4a
risapav <risapav@gmail.com>
2020-11-28 22:16:52 +0100

    update

--- examples/hdmi/top.sv
+++ examples/hdmi/top.sv
@@ -16,54 +16,54 @@ module top
 (
 // {ALTERA_ARGS_BEGIN} DO NOT REMOVE THIS LINE!
 
-	input  logic        clk,
-	output logic        sdram_clk,   // sdram_clk.clk
-	output logic [11:0] sdram_addr,  // sdram.addr
-	output logic [1:0]  sdram_ba,    // .ba
-	output logic        sdram_cas_n, // .cas_n
-	output logic        sdram_cke,   // .cke
-	output logic        sdram_cs_n,  // .cs_n
-	inout  logic [15:0] sdram_dq,    // .dq
-	output logic [1:0]  sdram_dqm,   // .dqm
-	output logic        sdram_ras_n, // .ras_n
-	output logic        sdram_we_n,  // .we_n
-	output logic  [0:4] led,
+	input          clk,
+	output         sdram_clk,   // sdram_clk.clk
+	output  [11:0] sdram_addr,  // sdram.addr
+	output  [1:0]  sdram_ba,    // .ba
+	output         sdram_cas_n, // .cas_n
+	output         sdram_cke,   // .cke
+	output         sdram_cs_n,  // .cs_n
+	inout   [15:0] sdram_dq,    // .dq
+	output  [1:0]  sdram_dqm,   // .dqm
+	output         sdram_ras_n, // .ras_n
+	output         sdram_we_n,  // .we_n
+	output   [0:4] led,
 	
 	
 	// user interface
-	input	logic	[0:2]	button,
+	input		[0:2]	button,
 
-	input logic P3_2,
-	input logic P3_3,
-	input logic P3_4,
-	input logic P4_2,
-	input logic P5_2,
-	input logic P5_3,
-	input logic P6_3,
-	input logic P6_2,
-	input logic P8_3,
-	input logic P8_4,
-	input logic P8_5,
-	input logic P8_6,
-	input logic P8_7,
-	input logic P8_8,
-	input logic P8_9,
-	input logic P8_10,
-	input logic P8_11,
-	input logic P8_12,
-	input logic P8_13,
-	input logic P8_14,
-	input logic P8_15,
-	input logic P8_16,
-	input logic SDA,
-	output	logic	SDC,
-	output	logic	uart_tx,
-	input logic uart_rx,
+//	input  P3_2,
+//	input  P3_3,
+//	input  P3_4,
+//	input  P4_2,
+//	input  P5_2,
+//	input  P5_3,
+//	input  P6_3,
+//	input  P6_2,
+//	input  P8_3,
+//	input  P8_4,
+//	input  P8_5,
+//	input  P8_6,
+//	input  P8_7,
+//	input  P8_8,
+//	input  P8_9,
+//	input  P8_10,
+//	input  P8_11,
+//	input  P8_12,
+//	input  P8_13,
+//	input  P8_14,
+//	input  P8_15,
+//	input  P8_16,
+//	input  SDA,
+//	output SDC,
+//	output uart_tx,
+//	input  uart_rx,
 	
 	// hdmi interface
 	// hdmi interface
-	output logic	[0:3]	tmds_p,
-	output logic	[0:3]	tmds_n
+	output [0:3]	tmds_p,
+	output [0:3]	tmds_n
 	
 // {ALTERA_ARGS_END} DO NOT REMOVE THIS LINE!
 
@@ -78,8 +78,8 @@ module top
 // {ALTERA_MODULE_BEGIN} DO NOT REMOVE THIS LINE!
 
 	//reset generator
-	wire	reset;
-	wire key;
+	logic	reset;
+	logic key;
 	
 	PushButton_Debouncer button_reset(
 		.clk,
@@ -97,12 +97,12 @@ module top
 	//end of reset
 	
 	//frequency generator
-	wire	clk_pixel;
-	wire	clk_pixel_x10;
-	wire	clk_audio;
-	wire	clk_atari;
+	logic	clk_pixel;
+	logic	clk_pixel_x10;
+	logic	clk_audio;
+	logic	clk_atari;
 
-	pll	clock(
+	pll clock(
 		.inclk0(clk),
 		.c0(clk_pixel),
 		.c1(clk_pixel_x10),
